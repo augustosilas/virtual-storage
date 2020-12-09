10d51e5f159ecb673f4a5c2c992a12aeb9cb1eaf
Silas Augusto <augusto.fernandes775@gmail.com>
2020-12-09 10:31:38 -0300

    structuring stores
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:virtual_store/tabs/home_tab.dart';
 import 'package:virtual_store/tabs/orders_tab.dart';
+import 'package:virtual_store/tabs/places_tab.dart';
 import 'package:virtual_store/tabs/products_tab.dart';
 import 'package:virtual_store/widgets/cart_button.dart';
 import 'package:virtual_store/widgets/custom_drawer.dart';
@@ -28,14 +29,23 @@ class HomeScreen extends StatelessWidget {
           body: ProductsTab(),
           floatingActionButton: CartButton(),
         ),
-        Container(color: Colors.blue),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Lojas"),
+            centerTitle: true,
+          ),
+          body: PlacesTab(),
+          drawer: CustomDrawer(pageController: _pageController),
+        ),
         Scaffold(
           appBar: AppBar(
             title: Text("Meus Pedidos"),
             centerTitle: true,
           ),
           body: OrdersTab(),
-          drawer: CustomDrawer(pageController: _pageController,),
+          drawer: CustomDrawer(
+            pageController: _pageController,
+          ),
         )
       ],
     );
